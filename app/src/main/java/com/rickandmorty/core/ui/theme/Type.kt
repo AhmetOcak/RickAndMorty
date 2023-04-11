8e7ba593c1d4ca71dc367838f147d7c126779331
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-04-11 19:40:34 +0300

    rick and morty font family added

--- app/src/main/java/com/rickandmorty/core/ui/theme/Type.kt
+++ app/src/main/java/com/rickandmorty/core/ui/theme/Type.kt
@@ -1,6 +1,9 @@
 package com.rickandmorty.core.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -8,12 +11,25 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.rickandmorty.R
 
+private val pageTitleGradient = listOf(
+    Color(0xFF39FF14),
+    Color(0xFF88FF0C),
+    Color(0xFFB0FF08),
+    Color(0xFFD7FF04),
+    Color(0xFFFFFF00)
+)
+
 private val Avenir = FontFamily(
     Font(R.font.avenir_lts_td_black, FontWeight.Bold),
     Font(R.font.avenir_lts_td_roman, FontWeight.SemiBold),
     Font(R.font.avenir_lts_td_roman, FontWeight.Normal)
 )
 
+private val RickAndMorty = FontFamily(
+    Font(R.font.get_schwifty, FontWeight.Bold)
+)
+
+@OptIn(ExperimentalTextApi::class)
 val Typography = Typography(
     titleLarge = TextStyle(
         fontFamily = Avenir,
@@ -29,5 +45,21 @@ val Typography = Typography(
         fontFamily = Avenir,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Avenir,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = RickAndMorty,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        brush = Brush.horizontalGradient(pageTitleGradient)
+    ),
+    displayMedium = TextStyle(
+        fontFamily = RickAndMorty,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp
     )
 )
