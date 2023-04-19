7955d899ef372ef5b59c278a1e952e6d9a621c01
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2023-04-19 23:29:20 +0300

    dispatcher injected

--- app/src/main/java/com/rickandmorty/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/rickandmorty/presentation/home/HomeViewModel.kt
@@ -11,76 +11,88 @@ import com.rickandmorty.domain.usecase.character.GetCharacterUseCase
 import com.rickandmorty.presentation.utils.CharacterGender
 import com.rickandmorty.presentation.utils.EXCEPTION_MESSAGE
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val getCharacterUseCase: GetCharacterUseCase,
-    locationRepository: LocationRepository
+    locationRepository: LocationRepository,
+    private val ioDispatcher: CoroutineDispatcher
 ) : ViewModel() {
 
     private val _characterState = MutableStateFlow<CharacterState>(CharacterState.Loading)
     val characterState = _characterState.asStateFlow()
 
-    private val _locations =  locationRepository.getLocations().cachedIn(viewModelScope)
+    private val _locations = locationRepository.getLocations().cachedIn(viewModelScope)
     val locations = _locations
 
-    fun getCharacters(residents: ArrayList<String>) = viewModelScope.launch(Dispatchers.IO) {
-        val characterIds = arrayListOf<Int>()
+    fun getCharacters(residents: ArrayList<String>) {
+        viewModelScope.launch(ioDispatcher) {
+            val characterIds = arrayListOf<Int>()
 
-        residents.forEach {
-            try {
-                characterIds.add(
-                    extractCharacterId(it)
-                )
-            } catch (e: Exception) {
-                _characterState.value = CharacterState.Error(message = EXCEPTION_MESSAGE)
+            residents.forEach {
+                try {
+                    characterIds.add(
+                        extractCharacterId(it)
+                    )
+                } catch (e: Exception) {
+                    _characterState.value = CharacterState.Error(message = EXCEPTION_MESSAGE)
+                }
             }
-        }
 
-        // Eğer characterIds boş ise, seçili lokasyonda resident yok demektir.
-        if (characterIds.isEmpty()) {
-            _characterState.value = CharacterState.Success(data = arrayListOf())
-        } else {
-            getCharacterUseCase(characterIds).collect() { response ->
-                when (response) {
-                    is Response.Loading -> {
-                        _characterState.value = CharacterState.Loading
-                    }
-                    is Response.Success -> {
-                        _characterState.value = CharacterState.Success(data = response.data)
-                    }
-                    is Response.Error -> {
-                        _characterState.value = CharacterState.Error(message = response.message)
+            // Eğer characterIds boş ise, seçili lokasyonda resident yok demektir.
+            if (characterIds.isEmpty()) {
+                _characterState.value = CharacterState.Success(data = arrayListOf())
+            } else {
+                getCharacterUseCase(characterIds)
+                    .flowOn(ioDispatcher)
+                    .collect() { response ->
+                    when (response) {
+                        is Response.Loading -> {
+                            _characterState.value = CharacterState.Loading
+                        }
+
+                        is Response.Success -> {
+                            _characterState.value = CharacterState.Success(data = response.data)
+                        }
+
+                        is Response.Error -> {
+                            _characterState.value = CharacterState.Error(message = response.message)
+                        }
                     }
                 }
             }
         }
     }
 
-    private fun extractCharacterId(residentUrl: String): Int {
+    fun extractCharacterId(residentUrl: String): Int {
         val uri = Uri.parse(residentUrl)
         return uri.lastPathSegment?.toInt() ?: throw Exception()
     }
 
     fun setCharacterGenderImage(gender: String): Int {
-        return when(gender) {
+        return when (gender) {
             CharacterGender.MALE -> {
                 R.drawable.male
             }
+
             CharacterGender.FEMALE -> {
                 R.drawable.female
             }
+
             CharacterGender.GENDERLESS -> {
                 R.drawable.genderless
             }
+
             CharacterGender.UNKNOWN -> {
                 R.drawable.unknown
             }
+
             else -> {
                 R.drawable.unknown
             }
